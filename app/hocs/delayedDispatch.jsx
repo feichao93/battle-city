b00046d90fee0522d335f2586a8c5c92990dc726
shinima <842351815@qq.com>
2017-03-06 21:42:21 +0800

    添加注释

--- app/hocs/delayedDispatch.jsx
+++ app/hocs/delayedDispatch.jsx
@@ -3,6 +3,21 @@ import { connect } from 'react-redux'
 import wrapDisplayname from 'recompose/wrapDisplayName'
 import * as selectors from 'utils/selectors'
 
+// HOC. 用来延迟dispatch action
+// 组件将connect到store, 故每次TICK时高阶组件都会更新
+// 在组件被mount指定的时间之后(由参数delay指定), 下一次did-update时将dispatch对应的action
+// action通过props传入, 默认prop名称为 'delayedAction', 可以通过参数propKey来改变
+// 参数delay - 延迟时间(单位:毫秒)
+// 参数propKey - 需要dispatch的action所对应的prop名称
+//
+// 例如:
+// 组件Explosion定义如下:
+// @delayedDispatch(200)
+// class Explosion { ... }
+// 组件Explosion使用如下:
+// <Explosion x={...} y={...} delayedAction={{ type: REMOVE_EXPLOSION, ... }} />
+// }
+// 这样可以实现<Explosion />自动销毁的功能了
 export default function delayedDispatch(delay, propKey = 'delayedAction') {
   return function (BaseComponent) {
     class Component extends React.Component {
@@ -15,8 +30,12 @@ export default function delayedDispatch(delay, propKey = 'delayedAction') {
 
       constructor(props) {
         super(props)
-        this.startTime = props.time
+        this.startTime = 0
         this.dispatched = false
+      }
+
+      componentDidMount() {
+        this.startTime = this.props.time
       }
 
       componentDidUpdate() {
--- app/hocs/registerTick.jsx
+++ app/hocs/registerTick.jsx
@@ -4,6 +4,13 @@ import { connect } from 'react-redux'
 import * as _ from 'lodash'
 import * as selectors from 'utils/selectors'
 
+// HOC. 用来向组件注入名为 'tickIndex' 的prop
+// tickIndex会随着时间变化
+// 提供不同的interval可以改变tickIndex变化速度和tickIndex的范围
+// 例如, intervals为 [100, 200, 300]
+// 则前100毫秒中tickIndex的值为0, 接下来的200毫秒中tickIndex的值为1,
+// 紧接着的300毫秒中tickIndex的值为2. 然后tickIndex又会变为0, 如此循环...
+// tickIndex的值为 i 的时间长度由itnervals数组下标 i 对应的数字决定
 export default function registerTick(...intervals) {
   const sum = _.sum(intervals)
   return function (BaseComponent) {
